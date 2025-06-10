cd021352e8ddd258913b13dd5ce18bc7ed69bc9c
Tejas Babar <tejasbabar335@gmail.com>
2025-06-10 23:46:03 +0530

    Create selection_sort.java

---
+++ selection_sort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class selection_sort{
+   
+    public static void selection_sort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int minPost=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[minPost]>arr[j]){
+                    minPost=j;
+                }
+            }
+            int temp = arr[minPost];
+            arr[minPost]=arr[i];
+            arr[i]=temp;
+        }
+    }
+
+
+    public static void print_array(int arr[]){
+       for(int i=0;i<arr.length;i++){
+           System.out.print(arr[i]+" ");
+      }
+    }
+
+    public static void main(String args[]){
+      int arr[]={8,6,4,7,6,3};
+      selection_sort(arr);
+      print_array(arr);
+    }
+}
