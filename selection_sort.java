1661ae0ed12eb875ca353a315c834ce134230d18
Tejas Babar <tejasbabar335@gmail.com>
2025-06-11 14:38:32 +0530

    Update selection_sort.java

--- selection_sort.java
+++ selection_sort.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class selection_sort{
    
-    public static void selection_sort(int arr[]){
+    public static void selectionSort(int arr[]){
         for(int i=0;i<arr.length-1;i++){
             int minPost=i;
             for(int j=i+1;j<arr.length;j++){
