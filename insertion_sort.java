fb8f2e7eebb1704040c51d24608fa973f0b6d30b
Tejas Babar <tejasbabar335@gmail.com>
2025-06-11 14:39:29 +0530

    Create insertion_sort.java

---
+++ insertion_sort.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class insertion_sort{
+
+    public static void insertionSort(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while(prev >=0 && arr[prev]> curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=curr;
+        }
+    }
+
+        public static void print_array(int arr[]){
+       for(int i=0;i<arr.length;i++){
+           System.out.print(arr[i]+" ");
+      }
+    }
+
+    public static void main(String[] args) {
+        int arr[]={1,3,5,6,8,9,2};
+        insertionSort(arr);
+              print_array(arr);
+
+    }
+
+
+}
